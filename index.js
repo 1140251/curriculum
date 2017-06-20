e3de9827835ce898269394a720e07dde4f0b7bb7
Daniel Maia Dias <daniel.maia@ymedia.pt>
2017-06-20 19:48:14 +0100

    first window without text and background image

---
+++ index.js
@@ -0,0 +1,9 @@
+setTimeout(function()
+    {
+        document.getElementById("load").style.display = "none";
+        document.getElementById("name").style.display = "inline";
+        document.getElementById("developer").style.display = "inline";
+        document.getElementById("monitor-div").style.display = "inline";
+        document.getElementById("screen").style.display = "inline";
+    },
+    5000);
