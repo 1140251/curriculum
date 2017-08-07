0a07d55564614dcaeac54d851b567ade556bd152
Daniel Maia Dias <daniel.maia@ymedia.pt>
2017-08-07 23:16:33 +0100

    change top calendar and projector

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var slideIndex = 1;
-
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-}
-
 
 window.onresize = function (event) {
     applyOrientation();
@@ -23,17 +17,10 @@ function setCalendar() {
 
 function applyOrientation(x) {
     if (window.innerHeight > window.innerWidth) {
-        if (isMobileDevice()) {
-            document.getElementById("end-mobile").style.display = "inline";
-        } else {
-            document.getElementById("end-computer").style.display = "inline";
-        }
         document.getElementById("load-div").style.display = "none";
         document.getElementById("calendar-div").style.display = "none";
         document.getElementById("developer").style.display = "none";
         document.getElementById("monitor-div").style.display = "none";
-        document.getElementById("slider").style.display = "none";
-        document.getElementById("window").style.display = "none";
         x = true;
     } else {
         document.getElementById("load").setAttribute("src", "load.svg?dummy=" + new Date().getTime());
@@ -44,10 +31,7 @@ function applyOrientation(x) {
                     document.getElementById("developer").style.display = "inline";
                     document.getElementById("monitor-div").style.display = "inline";
                     document.getElementById("calendar-div").style.display = "inline";
-                    document.getElementById("slider").style.display = "inline";
-                    document.getElementById("window").style.display = "inline";
                     setCalendar();
-                    setSlider(false);
                 },
                 5000);
             x = true;
@@ -56,9 +40,7 @@ function applyOrientation(x) {
             document.getElementById("developer").style.display = "inline";
             document.getElementById("monitor-div").style.display = "inline";
             document.getElementById("calendar-div").style.display = "inline";
-            document.getElementById("slider").style.display = "inline";
-            document.getElementById("window").style.display = "inline";
-            setSlider(true);
+
         }
     }
 }
@@ -67,39 +49,39 @@ window.onload = function () {
     var x = false;
     applyOrientation(x);
 
-    var otherDate= new Date();
-    var birthDate= new Date("1995-10-05");
-    var years = (otherDate.getFullYear() - birthDate.getFullYear());
-    if (otherDate.getMonth() < birthDate.getMonth() ||
-        otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
-        years--;
-    }
-    document.getElementById("text-age").textContent = document.getElementById("text-age").textContent.replace("{age}", years);
+    // var otherDate= new Date();
+    // var birthDate= new Date("1995-10-05");
+    // var years = (otherDate.getFullYear() - birthDate.getFullYear());
+    // if (otherDate.getMonth() < birthDate.getMonth() ||
+    //     otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+    //     years--;
+    // }
+    //document.getElementById("text-age").textContent = document.getElementById("text-age").textContent.replace("{age}", years);
 };
 
-
-function setSlider(x) {
-    showDivs(slideIndex);
-}
-
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("slide-div");
-    if (n > x.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = x.length
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
-}
-
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
+//
+// function setSlider(x) {
+//     showDivs(slideIndex);
+// }
+//
+// function showDivs(n) {
+//     var i;
+//     var x = document.getElementsByClassName("slide-div");
+//     if (n > x.length) {
+//         slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = x.length
+//     }
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+//     x[slideIndex - 1].style.display = "block";
+// }
+//
+// function plusDivs(n) {
+//     showDivs(slideIndex += n);
+// }
 
 
 
