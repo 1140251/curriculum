974bda76c4ff25b63ae5513f7f58b8ee93a6467a
Daniel Maia Dias <1140251@isep.ipp.pt>
2019-06-22 14:19:47 +0100

    no message

--- index.js
+++ index.js
@@ -1,88 +1,53 @@
 
-window.onresize = function (event) {
-    applyOrientation();
-};
-
-function setCalendar() {
-    var calendar = document.getElementById("calendar");
-    calendar.addEventListener("load", function () {
-        calendar.contentDocument.getElementById("day").textContent = (new Date().getUTCDate()<10 ? '0':'') + new Date().getUTCDate().toString();
-        var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-        ];
-        calendar.contentDocument.getElementById("month").textContent = monthNames[new Date().getMonth()];
-    }, false);
-}
-
-
-function applyOrientation(x) {
-    if (window.innerHeight > window.innerWidth) {
-        document.getElementById("load-div").style.display = "none";
-        document.getElementById("calendar-div").style.display = "none";
-        document.getElementById("developer").style.display = "none";
-        document.getElementById("monitor-div").style.display = "none";
-        x = true;
-    } else {
-        document.getElementById("load").setAttribute("src", "load.svg?dummy=" + new Date().getTime());
-
-        if (x == false) {
-            setTimeout(function () {
-                    document.getElementById("load-div").style.display = "none";
-                    document.getElementById("developer").style.display = "inline";
-                    document.getElementById("monitor-div").style.display = "inline";
-                    document.getElementById("calendar-div").style.display = "inline";
-                    setCalendar();
-                },
-                5000);
-            x = true;
-        } else {
-            document.getElementById("load-div").style.display = "none";
-            document.getElementById("developer").style.display = "inline";
-            document.getElementById("monitor-div").style.display = "inline";
-            document.getElementById("calendar-div").style.display = "inline";
-
-        }
-    }
-}
 
 window.onload = function () {
-    var x = false;
-    applyOrientation(x);
-
-    // var otherDate= new Date();
-    // var birthDate= new Date("1995-10-05");
-    // var years = (otherDate.getFullYear() - birthDate.getFullYear());
-    // if (otherDate.getMonth() < birthDate.getMonth() ||
-    //     otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
-    //     years--;
-    // }
-    //document.getElementById("text-age").textContent = document.getElementById("text-age").textContent.replace("{age}", years);
+  var x = false;
+  var otherDate = new Date();
+  var birthDate = new Date("1995-10-05");
+  var years = (otherDate.getFullYear() - birthDate.getFullYear());
+  if (otherDate.getMonth() < birthDate.getMonth() ||
+    otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+    years--;
+  }
+  document.getElementById("text-age").textContent = document.getElementById("text-age").textContent.replace("{{age}}", years);
 };
 
-//
-// function setSlider(x) {
-//     showDivs(slideIndex);
-// }
-//
-// function showDivs(n) {
-//     var i;
-//     var x = document.getElementsByClassName("slide-div");
-//     if (n > x.length) {
-//         slideIndex = 1
-//     }
-//     if (n < 1) {
-//         slideIndex = x.length
-//     }
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";
-//     }
-//     x[slideIndex - 1].style.display = "block";
-// }
-//
-// function plusDivs(n) {
-//     showDivs(slideIndex += n);
-// }
 
+document.onkeydown = function (event) {
+  switch (event.keyCode) {
+    case 37:
+      moveLeft();
+      break;
+    case 39:
+      moveRight();
+      break;
+  }
+};
 
+var speed = 10, // the box will move by 10 pixels on every step
+  direction = 1; // 1 moves in the positive direction; -1 vice versa
 
-
+function moveLeft() {
+  var boxElement = document.getElementById('motorcycle');
+  var boxLeftPos = boxElement.offsetLeft,
+    boxRightPos = boxLeftPos + boxElement.offsetWidth;
+  direction = -1;
+  // When left side of the box goes too far - change direction:
+  if (boxLeftPos < 0) {
+    return;
+  }
+  // Recalculate position:
+  boxElement.style.left = (boxLeftPos + speed * direction) + 'px';
+}
+function moveRight() {
+  var boxElement = document.getElementById('motorcycle');
+  var boxLeftPos = boxElement.offsetLeft,
+    boxRightPos = boxLeftPos + boxElement.offsetWidth;
+  // When right side of the box goes too far - change direction:
+  if (boxRightPos > document.body.offsetWidth) {
+    return;
+  }
+  direction = 1;
+  // Recalculate position:
+  boxElement.style.left = (boxLeftPos + speed * direction) + 'px';
+}
